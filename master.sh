ab542285e1320e044e456fd1c8c5133b7062aaab
guru-1984 <84213875+guru-1984@users.noreply.github.com>
2021-08-05 18:33:34 +0530

    done

--- master.sh
+++ master.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get update -y
 sudo apt-get install openjdk-8-jdk openjdk-8-jre -y
+chmod 777 /home/guru/.bashrc
 sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /home/guru/.bashrc
 sudo echo 'export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH' >> /home/guru/.bashrc
 #sudo chmod 777 /etc/environment
