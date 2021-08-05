ab091e9da63f150bbaa5d66f9995bcc4ad9fc3d1
guru-1984 <84213875+guru-1984@users.noreply.github.com>
2021-08-05 18:33:56 +0530

    test

--- node.sh
+++ node.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 sudo apt-get update -y 
 sudo apt-get install openjdk-8-jdk openjdk-8-jre -y
+chmod 777 /home/guru/.bashrc
+sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /home/guru/.bashrc
+sudo echo 'export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH' >> /home/guru/.bashrc
 #sudo chmod 777 /etc/environment
 #sudo echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/environment
 #sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre' >> /etc/environment
