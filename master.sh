8e68bd20bde2b52eba7a77d729d6384ef44532ad
guru-1984 <84213875+guru-1984@users.noreply.github.com>
2021-08-05 18:35:42 +0530

    done

--- master.sh
+++ master.sh
@@ -9,6 +9,6 @@ sudo echo 'export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH' >> /home/gur
 #sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre' >> /etc/environment
 #wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 #sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-wget -P /home/guru https://get.jenkins.io/war-stable/2.289.2/jenkins.war && java  -jar /home/guru/jenkins.war > /home/guru/jenkinoutpu
+wget -P /home/guru https://get.jenkins.io/war-stable/2.289.2/jenkins.war && java  -jar /home/guru/jenkins.war > /home/guru/jenkinoutpu &
 #sudo apt-get update
 #sudo apt-get install jenkins -y
